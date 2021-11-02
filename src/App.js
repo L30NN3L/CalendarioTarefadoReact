b5cb269733795f1aa8155037f3376334369466ee
Hugo Leonel <leonnelcode@gmail.com>
2021-11-02 09:54:37 -0300

    Ajuste Merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
-=======
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
->>>>>>> origin/main
 import Dia from './Dia';
 import './calendario.css'
 import Cabecalho from './Cabecalho';
